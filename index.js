53b1d0691fbbffed296a0b9492aecd54c3179147
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-08-14 16:44:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(holiday name, number) {
+function holidayCountdown(name, number) {
   return `It's ${days} days until ${holiday}!`
 }
